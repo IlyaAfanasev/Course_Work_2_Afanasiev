5467f15fa18f650d463d5a5a00232e6ce5dc0c6d
IlyuAfanasev <gecktor.86@mail.ru>
2023-07-16 19:15:14 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/course_work_2_afanasiev/CourseWork2AfanasievApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course_work_2_afanasiev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2AfanasievApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2AfanasievApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/course_work_2_afanasiev/CourseWork2AfanasievApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course_work_2_afanasiev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2AfanasievApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
